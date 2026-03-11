f6559aec6f7039c53b031b3fc078f98b3771520a
JoaoVictor-99 <joaosuemia@gmail.com>
2026-03-11 17:05:29 -0300

    Add files via upload

---
+++ Questão 1.py
@@ -0,0 +1,11 @@
+salario = {}
+
+print("\n Dados")
+print("Salário base: R$3.500 ")
+print("Você recebeu um bônus de R$800")
+salario = 3500 + 800
+print(salario)
+
+print("Você teve um desconto de R$250")
+Resultado = salario - 250
+print(Resultado)
---
+++ questao 2.py
@@ -0,0 +1,9 @@
+km = 450
+consumo= 8
+preço = 5.50
+resultado = 450 / 8
+ 
+km / consumo 
+km / consumo = preço * resultado
+
+
