80ae426d1a04b9011c6fc48c47d2627f8b69bfe3
JoaoVictor-99 <joaosuemia@gmail.com>
2026-03-11 17:25:57 -0300

    Add files via upload

---
+++ questão 2.py
@@ -0,0 +1,8 @@
+km = 450
+litros = 5.50
+
+consumo = km / 8
+gasto = consumo * litros
+
+print("Consumo: ", consumo, "Litros")
+print("Gasto:", gasto)
---
+++ questão 3.py
@@ -0,0 +1,9 @@
+Estoque1 = 100
+Estoque2 = 100 + 50
+Estoque3 = 150 - 30
+Estoque4 = 120 - 5
+
+print("Seu estoque base: ", Estoque)
+print("Seu estoque após Re-estoque: ", Estoque2)
+print("Seu estoque depois das vendas: ", Estoque3)
+print("Seu estoque após as unids devolvidas: ", Estoque)
